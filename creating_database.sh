2126e3830f72a14bc3103a91b2329f72c1d9f807
asmaa0462 <asmaatarek06@gmail.com>
2024-08-11 05:53:38 +0300

    Create Database

---
+++ creating_database.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#creating a new database by checking two things
+# 1- its existence in my Database system
+# 2- a valid name
+
+echo "Enter the database name: " 
+read DataBaseName
+
+#checking the name validity using regex
+while ! [[ $DataBaseName =~ ^[a-zA-Z_][a-zA-Z0-9_\$\@#]*$ ]]; do
+	echo "Enter a valid name"
+	read -p "Enter the database name :  " DataBaseName
+done
+
+#checking the existence of the database 
+if [[ -d ./MyDataBases/$DataBaseName ]]; then
+	echo "This name already exists, choose another name"
+	. ./creating_database.sh
+else
+	mkdir ./MyDataBases/"$DataBaseName"
+	echo "DataBase: $DataBaseName was created Successfully"
+fi
